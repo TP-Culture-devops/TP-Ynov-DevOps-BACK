a5f50e4c20db7f043fd6963e291106ba3977b5fe
Julien <julien.guilet@etu.univ-grenoble-alpes.fr>
2022-11-03 11:16:13 +0100

    Ajout du service getFileContentOf

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Bienvenue sur le back !')
+})
+
+/*
+Renvoie le contenu du fichier mis dans le paramètre nom.
+Le fichier doit être présent dans le dossier Files
+*/
+app.get('/getFileContentOf', (req, res) => {
+    res.download("./files/"+req.query.nom)
 })
 
 app.listen(port, () => {
