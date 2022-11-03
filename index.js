1743c93e7c8b43d5c391b5403729e263977f0004
Keemoh <vuepierre@gmail.com>
2022-11-03 15:32:50 +0100

    requete insertion new file

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
-const dotenv = require('dotenv');
-const {executeFilesCrudOperations} = require('./services/mongoService');
-const {ObjectId} = require("mongodb");
+const dotenv = require("dotenv");
+const { executeFilesCrudOperations } = require("./services/mongoService");
+const { ObjectId } = require("mongodb");
 dotenv.config();
-const cors = require('cors')
-const fs = require('fs')
+const cors = require("cors");
+const fs = require("fs");
 
-let files = null
+let files = null;
 
-executeFilesCrudOperations().then(
-    fileCollection => files = fileCollection
-)
+executeFilesCrudOperations().then((fileCollection) => (files = fileCollection));
 
-const express = require('express');
-const console = require('console');
-const app = express()
-const port = 3000
+const express = require("express");
+const console = require("console");
+const app = express();
+const port = 3000;
 app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Bienvenue sur le back !')
-})
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur le back !");
+});
 
 /*
 Renvoie tous les fichiers en base
 */
-app.get('/files/', async (req, res) =>  {
-    res.send(await files.find().toArray())
-})
+app.get("/files/", async (req, res) => {
+  res.send(await files.find().toArray());
+});
 
-app.get('/ContentOf/', async (req, res) =>  {
-    let fileContent;
-    fs.readFile("./"+process.env.DATA+"/"+req.query.id, 'utf8', function (err,data) {
-    if (err) {
+app.get("/ContentOf/", async (req, res) => {
+  let fileContent;
+  fs.readFile(
+    "./" + process.env.DATA + "/" + req.query.id,
+    "utf8",
+    function (err, data) {
+      if (err) {
         return console.log(err);
+      }
+      res.send(data);
     }
-    res.send(data )
-    });
-    
-})
+  );
+});
 
-app.get('/download/', async (req, res) =>  {
-    res.download("./files/"+req.query.id )
-})
+app.get("/Add/", async (req, res) => {
+  await files.insertOne(req.body.path, req.body.description);
+  var file = files.findOne({ path: req.body.path });
+  fs.appendFile(file._id, req.body.content, function (err) {
+    if (err) throw err;
+    res.send("Ajout reussi !");
+  });
+});
+
+app.get("/download/", async (req, res) => {
+  res.download("./files/" + req.query.id);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
